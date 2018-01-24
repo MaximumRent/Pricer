f3cef70054a71786eaea5a6be52fdf5949ebbeb1
Maxim Petrusevich <maks1464@gmail.com>
2018-01-24 20:59:57 +0300

    Add simple HTML reader

---
+++ com/megapapa/src/main/util/html_reader.go
@@ -0,0 +1,22 @@
+package util
+
+import (
+	"net/http"
+	"io/ioutil"
+)
+
+const ERROR_GET_MSG = "Can't do GET request to current URL: "
+
+func GetHtml(url string) (string, error) {
+	response, err := http.Get(url)
+	if err != nil {
+		return ERROR_GET_MSG + url, err;
+	}
+	defer response.Body.Close()
+	bytes, err := ioutil.ReadAll(response.Body)
+	if err != nil {
+		return ERROR_GET_MSG + url, err;
+	}
+	html := string(bytes)
+	return html, nil;
+}
